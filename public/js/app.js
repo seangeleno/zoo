ba65559b87afba8193d3ab1297a4b5295dffbf84
Sean Esteva <seangeleno@gmail.com>
2015-12-09 11:16:03 -0800

    almost got it to work

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,10 @@
   function zooController(){
     var self = this //allows us to use $scope and references the controller itself,
     self.name = 'animalStyle' //creating properties of the zooController
+    self.animal = [{name: "Emma", type:"Seahorse", visible: true}, {name: "Andy", type:"Gorilla", visible: false}, {name: "Ted", type:"Bear", visible: true}]
+    self.changeVisibility = function(animal){
+      animal.visible = !animal.visible
+    }
     console.log(self.name);
 
   }
