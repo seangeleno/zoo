80e419e2b5c7edb54606d08afe919583f1958e74
Sean Esteva <seangeleno@gmail.com>
2015-12-09 10:46:02 -0800

    added app.js

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular.module('zooApp', ['ngRoute'])
+  angular.module('zooApp')
+    .controller('zooController', zooController)
+}());
+
+var zooAnimals = [{name: 'jimmy' type:'tiger'}]
